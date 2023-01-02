8eefa3e8109c05077ac2dfc5b269610365d00e5c
gen740 <genfujimoto@gmail.com>
2023-01-02 11:53:18 +0900

    Update

--- include/autodiff/autodiff.hpp
+++ include/autodiff/autodiff.hpp
@@ -5,6 +5,29 @@
 
 namespace autodiff {
 
+template <class T>
+concept Dual = requires(T& x) {
+                 { x* x } -> std::same_as<T>;
+               };
+
+constexpr int_fast32_t factorial_(int_fast32_t n) {
+  int_fast32_t ret{1};
+  for (int i = 1; i <= n; i++) {
+    ret *= i;
+  }
+  return ret;
+}
+
+constexpr int_fast32_t permutation_(int_fast32_t n, int_fast32_t m) {
+  int_fast32_t den{1};
+  int_fast32_t num{1};
+  for (int i = 0; i < m; i++) {
+    num *= n - i;
+    den *= (i + 1);
+  }
+  return num / den;
+}
+
 template <int N = 2, class T = double>
   requires std::floating_point<T>
 class dual {
@@ -12,7 +35,7 @@ class dual {
   using ValType = T;
   std::array<T, N + 1> value_{0};
 
-  dual(std::array<T, N + 1> value) : value_(value) {}
+  dual(const std::array<T, N + 1>& value) : value_(value) {}
   dual(std::array<T, N + 1>&& value) : value_(std::move(value)) {}
 
  public:
@@ -26,15 +49,13 @@ class dual {
   friend dual<N, T> operator*(dual<N, T> a, dual<N, T> b) {
     std::array<T, N + 1> v{0};
 
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < N + 1; i++) {
       for (int j = 0; j < i + 1; j++) {
-        std::cout << i << ": " << j << "," << i - j << std::endl;
-        std::cout << a.value_[j] * b.value_[i - j] << std::endl;
-        v[i] += a.value_[j] * b.value_[i - j];
+        v[i] += permutation_(i, i - j) * a.value_[j] * b.value_[i - j];
       }
     }
 
-    return v;
+    return std::move(v);
   }
 };
 
--- tests/autodiff_test.cc
+++ tests/autodiff_test.cc
@@ -3,13 +3,29 @@
 #include <autodiff/autodiff.hpp>
 #include <iostream>
 
+using autodiff::dual;
+
 template <class T>
-T func(T a) {
-  return a * a;
+void func1(T a) {
+  std::cout << a << std::endl;
 }
 
-TEST(autodiff, dummy) {
-  std::cout << func<autodiff::dual<3>>(3).value()[2] << std::endl;
+// template <template <class> class T>
+// void call(std::function<void(T<int>)> fun) {
+//   fun(3);
+// }
+
+TEST(autodiff, multiple) {
+  auto quad_func = []<autodiff::Dual T>(T a) -> T { return a * a; };
+
+  auto [d0, d1, d2, d3] = quad_func(dual<3>(3)).value();
+
+  EXPECT_EQ(d0, 9);
+  EXPECT_EQ(d1, 6);
+  EXPECT_EQ(d2, 2);
+  EXPECT_EQ(d3, 0);
+
+  // std::cout << quad_func(dual<3>(3)).value()[3] << std::endl;
   // std::cout << func<int>(3) << std::endl;
   // std::cout << autodiff::derivative<1, double>(func, 3) << std::endl;
   // std::cout << autodiff::derivative(func, 3) << std::endl;
