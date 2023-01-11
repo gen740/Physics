bb3d223ed114bc5c381a74cb7a395b02a447217a
gen740 <genfujimoto@gmail.com>
2023-01-11 10:49:55 +0900

    modified operator<<

--- include/fraction/Fraction.h
+++ include/fraction/Fraction.h
@@ -105,10 +105,8 @@ struct frac {
   frac &operator=(frac &&frac) noexcept = default;
   frac &operator=(const frac &frac) = default;
 
-  // TODO(gen740): Ubuntu build would be failed by ODR when frac<int> instantiation
-  template <std::signed_integral U>
   constexpr friend std::ostream &operator<<(std::ostream &os,
-                                            const frac<U> &frac) {
+                                            const frac<T> &frac) {
     if (frac.deno == 1) {
       os << "<frac:" << frac.nume << ">";
     } else {
